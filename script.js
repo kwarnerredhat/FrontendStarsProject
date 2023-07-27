530e1f85c513675eeb5cd3a6e9b557d60074f0ac
kwarnerredhat <kwarner@redhat.com>
2023-07-27 11:41:32 -0400

    Added title

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
       event.preventDefault();
       const rating = document.getElementById("rating").value;
       const reviewText = document.getElementById("review").value;
+      const title = document.getElementById("title").value;
   
       const reviewElement = document.createElement("div");
-      reviewElement.innerHTML = `<p>Rating: ${rating} stars</p><p>${reviewText}</p><hr>`;
+      reviewElement.innerHTML = `<p><strong>${title}</strong></p><p>Rating: ${rating} stars</p><p>${reviewText}</p><hr>`
       reviewsDiv.appendChild(reviewElement);
   
       reviewForm.reset();
